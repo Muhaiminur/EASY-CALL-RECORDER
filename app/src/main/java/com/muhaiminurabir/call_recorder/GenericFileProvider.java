056b9d4f809278819ee7c7d76aa8a44cd38bde72
Muhaiminur  Rahman <muhaiminurabir@gmail.com>
2019-02-11 18:11:07 +0600

    VERSION ONE

---
+++ app/src/main/java/com/muhaiminurabir/call_recorder/GenericFileProvider.java
@@ -0,0 +1,12 @@
+package com.muhaiminurabir.call_recorder;
+
+import android.support.v4.content.FileProvider;
+
+
+
+public class GenericFileProvider extends FileProvider {
+
+
+    public GenericFileProvider() {
+    }
+}
